d52a4800ed799504275f2934de032ece9ea7c0c8
mauro arias <mauro.arias@unity3d.com>
2022-06-07 10:41:15 -0400

    testing

--- scripts/Itest.sh
+++ scripts/Itest.sh
@@ -24,14 +24,15 @@ printTitleWithColor "building test docker image" "${yellow}"
 dockerBuild "${integrationTestPath}share-qa-libs/DockerfileItest" "itest:test" "--build-arg INTEGRATION_TEST_PATH=$integrationTestPath --build-arg TEST_ARG=$TEST_BUILD_ARG --no-cache --platform $dockerArch" || exitOnError "error generating docker test image"
 
 # run tests
-path=$(pwd)
-mkdir "${path}"/reporting
+reportingPath="$(pwd)${integrationTestPath}/reporting"
+printTitleWithColor "reporting in path $reportingPath" "${yellow}" 
+mkdir "${reportingPath}"
 printTitleWithColor "Running Itests" "${yellow}"
 getNetworkNameFromDockerCompose
-docker run -i --network="$networkName" --platform "$dockerArch" -v "${path}"/reporting:/reporting itest:test #|| printAlert "some tests fail, please check reporting"
+docker run -i --network="$networkName" --platform "$dockerArch" -v "${reportingPath}":/reporting itest:test #|| printAlert "some tests fail, please check reporting"
 
 # reporting logs
-docker-compose -f "${integrationTestPath}docker-compose.yml" logs -t > "${path}"/reporting/logs.log
+docker-compose -f "${integrationTestPath}docker-compose.yml" logs -t > "${reportingPath}"/logs.log
  
 # Stopping infra
 printTitleWithColor "Stopping infra" "${yellow}"
